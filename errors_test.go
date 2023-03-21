afcf1c2cebba3403e7e30a83eafc6ec4f3f85e23
Robert Sigler <sigler@improvisedscience.org>
2023-03-20 21:05:47 -0400

    Adding more error testing

--- errors_test.go
+++ errors_test.go
@@ -48,3 +48,66 @@ func TestErrBadStatusCodeNoInput(t *testing.T) {
 		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
 	}
 }
+
+func TestErrInvalidInput(t *testing.T) {
+	err := ErrInvalidInput{
+		Err: errors.New("Testing subError"),
+		Msg: "Testing ErrInvalidInput",
+	}
+	errorOutput := err.Error()
+	expectedOutput := "Testing ErrInvalidInput: Testing subError"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
+
+func TestErrInvalidInputNoInput(t *testing.T) {
+	err := ErrInvalidInput{}
+	errorOutput := err.Error()
+	expectedOutput := "input is not valid or nil"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
+
+func TestErrMissingInputField(t *testing.T) {
+	err := ErrMissingInputField{
+		Err: errors.New("Testing subError"),
+		Msg: "Testing ErrMissingInputField",
+	}
+	errorOutput := err.Error()
+	expectedOutput := "Testing ErrMissingInputField: Testing subError"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
+
+func TestErrMissingInputFieldNoInput(t *testing.T) {
+	err := ErrMissingInputField{}
+	errorOutput := err.Error()
+	expectedOutput := "missing input"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
+
+func TestErrUnexpectedResponse(t *testing.T) {
+	err := ErrUnexpectedResponse{
+		Err: errors.New("Testing subError"),
+		Msg: "Testing ErrUnexpectedResponse",
+	}
+	errorOutput := err.Error()
+	expectedOutput := "Testing ErrUnexpectedResponse: Testing subError"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
+
+func TestErrUnexpectedResponseNoInput(t *testing.T) {
+	err := ErrUnexpectedResponse{}
+	errorOutput := err.Error()
+	expectedOutput := "unexpected response"
+	if errorOutput != expectedOutput {
+		t.Errorf("Error() = %v, want %v", errorOutput, expectedOutput)
+	}
+}
